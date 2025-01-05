f529f541670a92d592feb0385075e5e52d397e64
Adu Bhandaru <abhandaru@x1.co>
2025-01-05 12:16:44 -0500

    added colors, css, cloudfront invalidation

--- eusphere-co/release/prod.sh
+++ eusphere-co/release/prod.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+# hand-roll the files that would be in `build`
 aws s3 cp ./index.html s3://eusphere/eusphere-co/index.html
 aws s3 sync ./static s3://eusphere/eusphere-co/static --delete
+
+# invalidate cloudfront cache
+aws cloudfront create-invalidation --distribution-id E2GD67ZBIK5701 --paths "/*"
