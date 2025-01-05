747966d19b631a9596d9d3f3968f0efec032a765
Adu Bhandaru <abhandaru@x1.co>
2025-01-05 13:30:17 -0500

    favicon support and origin alignment

--- eusphere-co/release/prod.sh
+++ eusphere-co/release/prod.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # hand-roll the files that would be in `build`
+aws s3 cp ./static/favicon.ico s3://eusphere/favicon.ico
 aws s3 cp ./index.html s3://eusphere/eusphere-co/index.html
 aws s3 sync ./static s3://eusphere/eusphere-co/static --delete
 
