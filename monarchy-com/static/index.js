e38a560c9a08036ba3613b133ab60b1e4778734f
Adu Bhandaru <abhandaru@x1.co>
2025-01-13 10:32:09 -0500

    ensure that carousels have mutually exclusive art pieces

--- monarchy-com/static/index.js
+++ monarchy-com/static/index.js
@@ -3,18 +3,28 @@ const root = document.getElementById("root");
 fetch("/static/concept.json")
   .then((response) => response.json())
   .then((data) => {
-    root.appendChild(mkCarousel(data, 25));
-    root.appendChild(mkCarousel(data, 15));
+    const splits = shuffleSplit(data, 2);
+    root.appendChild(mkCarousel(splits[0], 25));
+    root.appendChild(mkCarousel(splits[1], 15));
   });
 
-function mkCarousel(data, speed) {
+function mkCarousel(images, speed) {
   const carousel = document.createElement("div");
-  const paths = [...data.images].sort(() => Math.random() - 0.5);
   const animationStyle = `slide ${speed}s linear infinite`;
   // build the carousel
   carousel.classList.add("carousel");
-  carousel.innerHTML = paths
+  carousel.innerHTML = images
     .map((p) => `<img src="/static/concept/${p}" style="animation: ${animationStyle}">`)
     .join("");
   return carousel;
+}
+
+function shuffleSplit(data, count) {
+  const images = [...data.images].sort(() => Math.random() - 0.5);
+  const splits = [];
+  for (let i = 0; i < count; i++) {
+    const splitSize = Math.floor(images.length / (count - i));
+    splits.push(images.splice(0, splitSize));
+  }
+  return splits;
 }
