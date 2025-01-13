75c72d59c315746938801af500011472f6ee3760
Adu Bhandaru <abhandaru@x1.co>
2025-01-12 22:52:14 -0500

    add more concept art and improve deploy

--- monarchy-com/static/index.js
+++ monarchy-com/static/index.js
@@ -3,16 +3,13 @@ const root = document.getElementById("root");
 fetch("/static/concept.json")
   .then((response) => response.json())
   .then((data) => {
-    root.appendChild(mkCarousel(data));
-    root.appendChild(mkCarousel(data));
-    // root.appendChild(mkBanner());
-    root.appendChild(mkCarousel(data));
+    root.appendChild(mkCarousel(data, 25));
+    root.appendChild(mkCarousel(data, 15));
   });
 
-function mkCarousel(data) {
+function mkCarousel(data, speed) {
   const carousel = document.createElement("div");
   const paths = [...data.images].sort(() => Math.random() - 0.5);
-  const speed = 10 * Math.random();
   const animationStyle = `slide ${speed}s linear infinite`;
   // build the carousel
   carousel.classList.add("carousel");
@@ -20,11 +17,4 @@ function mkCarousel(data) {
     .map((p) => `<img src="/static/concept/${p}" style="animation: ${animationStyle}">`)
     .join("");
   return carousel;
-}
-
-function mkBanner() {
-  const banner = document.createElement("div");
-  banner.classList.add("banner");
-  banner.innerHTML = "Monarchy";
-  return banner;
 }
