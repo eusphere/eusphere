dba5330c7e59ea8336a3a21076f3aeb45e579ca9
Adu Bhandaru <abhandaru@x1.co>
2025-01-12 21:39:12 -0500

    add carousels to homepage

--- monarchy-com/static/index.js
+++ monarchy-com/static/index.js
@@ -1,2 +1,30 @@
-var root = document.getElementById("root");
-root.innerHTML = "Hello World";
+const root = document.getElementById("root");
+
+fetch("/static/concept.json")
+  .then((response) => response.json())
+  .then((data) => {
+    root.appendChild(mkCarousel(data));
+    root.appendChild(mkCarousel(data));
+    // root.appendChild(mkBanner());
+    root.appendChild(mkCarousel(data));
+  });
+
+function mkCarousel(data) {
+  const carousel = document.createElement("div");
+  const paths = [...data.images].sort(() => Math.random() - 0.5);
+  const speed = 10 * Math.random();
+  const animationStyle = `slide ${speed}s linear infinite`;
+  // build the carousel
+  carousel.classList.add("carousel");
+  carousel.innerHTML = paths
+    .map((p) => `<img src="/static/concept/${p}" style="animation: ${animationStyle}">`)
+    .join("");
+  return carousel;
+}
+
+function mkBanner() {
+  const banner = document.createElement("div");
+  banner.classList.add("banner");
+  banner.innerHTML = "Monarchy";
+  return banner;
+}
