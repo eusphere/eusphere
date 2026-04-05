e95b51ebda12e7ef69eb40e114c979b644924051
abhandaru <syphondu@gmail.com>
2026-04-05 14:58:35 -0400

    [connieadu] use cactus emoji as favicon

--- connieadu-com/release/prod.sh
+++ connieadu-com/release/prod.sh
@@ -3,6 +3,7 @@
 # hand-roll the files that would be in `build`
 aws s3 cp ./static/favicon.ico s3://eusphere/connieadu-com/favicon.ico
 aws s3 cp ./index.html s3://eusphere/connieadu-com/index.html
+aws s3 cp ./robots.txt s3://eusphere/connieadu-com/robots.txt
 aws s3 sync ./static s3://eusphere/connieadu-com/static --delete
 
 # invalidate cloudfront cache
